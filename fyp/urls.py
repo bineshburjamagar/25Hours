9b15fc87edb598c1463607375113791fcffe33e8
Binesh Burja Magar <bineshburzaa@gmail.com>
2022-03-11 23:09:22 +0545

    bootstrap installation

--- fyp/urls.py
+++ fyp/urls.py
@@ -19,6 +19,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('UserManagementSystem.urls')),
+    path('room', include('roomManagementSystem.urls')),
 
 ]
 
